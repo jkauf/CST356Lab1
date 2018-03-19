f7daeefb753e2adcb2aed5bc6eb9a9dfb5d59a87
Julian Kaufman <julian.kaufman@oit.edu>
2018-03-19 12:43:38 -0700

    Add project files.

---
+++ Lab1/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Lab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            Person person = new Person();
+            ViewBag.Title = "Person";
+            ViewBag.FirstName = person.FirstName = "Julian";
+            ViewBag.LastName = person.LastName = "Kaufman";
+
+            return View();
+        }
+    }
+}
